2418dfb08fcf161736965370de30f03bea9f20e3
Robert Theis <robert.m.theis@gmail.com>
2013-01-05 13:28:51 -0800

    add mips ABI

--- tess-two/jni/Application.mk
+++ tess-two/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := gnustl_static
-APP_ABI := armeabi armeabi-v7a x86 # mips has problems with gnustl_static
+APP_ABI := armeabi armeabi-v7a mips x86
 APP_OPTIM := release
 APP_PLATFORM := android-8
 APP_CPPFLAGS += -fexceptions -frtti
