ba92050840dbd8da911b96d4ca5fd6637bf7608f
Brian Hoffmann <github@slowpoke.de>
2015-10-29 20:42:47 +0100

    fix 64-bit ARMv8 build by applying patch from http://sourceforge.net/p/libpng/bugs/230/

--- tess-two/jni/libpng/pngpriv.h
+++ tess-two/jni/libpng/pngpriv.h
@@ -123,7 +123,7 @@
     * Note that gcc-4.9 defines __ARM_NEON instead of __ARM_NEON__, so we
     * check both variants.
     */
-#  if (defined(__ARM_NEON__) || defined(__ARM_NEON)) && \
+#  if defined(PNG_ARM_NEON) && (defined(__ARM_NEON__) || defined(__ARM_NEON)) && \
    defined(PNG_ALIGNED_MEMORY_SUPPORTED)
 #     define PNG_ARM_NEON_OPT 2
 #  else
