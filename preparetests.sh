ecb4219ceaa1dba01a834102e3275fce2996ffed
Robert Theis <robert.m.theis@gmail.com>
2015-12-13 17:47:49 -0800

    Modify build settings

--- preparetests.sh
+++ preparetests.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
-wget https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
+
+adb shell rm /mnt/sdcard/testAddPageToDocument.pdf
+adb shell rm /mnt/sdcard/testCreate.pdf
+
+if [ ! -f tesseract-ocr-3.02.eng.tar.gz ]; then
+    wget https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
+fi
 tar xvfz tesseract-ocr-3.02.eng.tar.gz
 adb shell mkdir /mnt/sdcard/tesseract
 adb shell mkdir /mnt/sdcard/tesseract/tessdata
+ 
+for f in tesseract-ocr/tessdata/eng.*; do
+    TRAINEDDATA_FILENAME="/mnt/sdcard/tesseract/tessdata/`basename $f`"
 
-adb push -p tesseract-ocr/tessdata/eng.traineddata /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.bigrams /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.fold /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.lm /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.nn /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.params /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.size /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.word-freq /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.tesseract_cube.nn /mnt/sdcard/tesseract/tessdata
+    if [ `adb shell "if [ -f $TRAINEDDATA_FILENAME ]; then echo 1; fi"` ]; then
+        echo "$TRAINEDDATA_FILENAME already present on device"
+        continue
+    fi 
+    
+    echo "Copying $f to device...";
+    adb push -p $f /mnt/sdcard/tesseract/tessdata
+done
+
+adb push -p tess-two/jni/com_googlecode_tesseract_android/src/tessdata/pdf.ttf /mnt/sdcard/tesseract/tessdata
+
+# adb shell am instrument -w com.googlecode.tesseract.android.test/android.test.InstrumentationTestRunner
--- tess-two/proguard-rules.pro
+++ tess-two/proguard-rules.pro
@@ -9,9 +9,28 @@
 
 # Add any project specific keep options here:
 
-# If your project uses WebView with JS, uncomment the following
-# and specify the fully qualified class name to the JavaScript interface
-# class:
-#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
-#   public *;
-#}
+-keep class com.googlecode.leptonica.android.Box {
+    private long mNativeBox;
+}
+-keep class com.googlecode.leptonica.android.Boxa {
+    private long mNativeBoxa;
+}
+-keep class com.googlecode.leptonica.android.Pix {
+    private long mNativePix;
+}
+-keep class com.googlecode.leptonica.android.Pixa {
+    private long mNativePixa;
+}
+-keep class com.googlecode.tesseract.android.TessBaseAPI {
+    private long mNativeData;
+    protected void onProgressValues(int, int, int, int, int, int, int, int, int);
+}
+-keep class com.googlecode.tesseract.android.PageIterator {
+    private long mNativePageIterator;
+}
+-keep class com.googlecode.tesseract.android.TessPdfRenderer {
+    private long mNativePdfRenderer;
+}
+-keep class com.googlecode.tesseract.android.ResultIterator {
+    private long mNativeResultIterator;
+}
