878f57b7a74ab1630ecbfeba9f9d3ec2c1ef914a
Robert Theis <robert.m.theis@gmail.com>
2016-03-09 21:51:53 -0800

    Switch to use clang

--- tess-two/jni/Application.mk
+++ tess-two/jni/Application.mk
@@ -3,4 +3,4 @@ APP_ABI := armeabi armeabi-v7a x86 mips arm64-v8a x86_64 mips64
 APP_OPTIM := release
 APP_PLATFORM := android-8
 APP_CPPFLAGS += -fexceptions -frtti
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := clang
