356fc96516f2bea22193b134aebdc9b87d65abfe
Robert Theis <robert.m.theis@gmail.com>
2016-09-28 20:27:31 -0700

    Update test setup scripts

--- preparetests.cmd
+++ preparetests.cmd
@@ -1,24 +1,20 @@
+git clone https://github.com/tesseract-ocr/tessdata.git
+
 adb shell rm /mnt/sdcard/testAddPageToDocument.pdf
 adb shell rm /mnt/sdcard/testCreate.pdf
 
 adb uninstall com.googlecode.tesseract.android.test
 
-powershell -Command "Invoke-WebRequest https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz -OutFile tesseract-ocr-3.02.eng.tar.gz"
-
-"C:\Program Files\7-Zip\7z.exe" e tesseract-ocr-3.02.eng.tar.gz -o"."
-"C:\Program Files\7-Zip\7z.exe" x tesseract-ocr-3.02.eng.tar -aoa
-del tesseract-ocr-3.02.eng.tar
-
 adb shell mkdir /mnt/sdcard/tesseract
 adb shell mkdir /mnt/sdcard/tesseract/tessdata
 
-adb push -p tesseract-ocr/tessdata/eng.cube.bigrams /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.fold /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.lm /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.nn /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.params /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.size /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.cube.word-freq /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.tesseract_cube.nn /mnt/sdcard/tesseract/tessdata
-adb push -p tesseract-ocr/tessdata/eng.traineddata /mnt/sdcard/tesseract/tessdata
+adb push -p tessdata/eng.cube.bigrams /mnt/sdcard/tesseract/tessdata
+adb push -p tessdata/eng.cube.fold /mnt/sdcard/tesseract/tessdata
+adb push -p tessdata/eng.cube.lm /mnt/sdcard/tesseract/tessdata
+adb push -p tessdata/eng.cube.nn /mnt/sdcard/tesseract/tessdata
+adb push -p tessdata/eng.cube.params /mnt/sdcard/tesseract/tessdata
+adb push -p tessdata/eng.cube.size /mnt/sdcard/tesseract/tessdata
+adb push -p tessdata/eng.cube.word-freq /mnt/sdcard/tesseract/tessdata
+adb push -p tessdata/eng.tesseract_cube.nn /mnt/sdcard/tesseract/tessdata
+adb push -p tessdata/eng.traineddata /mnt/sdcard/tesseract/tessdata
 adb push -p tess-two/jni/com_googlecode_tesseract_android/src/tessdata/pdf.ttf /mnt/sdcard/tesseract/tessdata
--- preparetests.sh
+++ preparetests.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
+git clone https://github.com/tesseract-ocr/tessdata.git
+
 adb shell rm /mnt/sdcard/testAddPageToDocument.pdf
 adb shell rm /mnt/sdcard/testCreate.pdf
 
 adb uninstall com.googlecode.tesseract.android.test
 
-if [ ! -f tesseract-ocr-3.02.eng.tar.gz ]; then
-    wget https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
-fi
-tar xvfz tesseract-ocr-3.02.eng.tar.gz
 adb shell mkdir /mnt/sdcard/tesseract
 adb shell mkdir /mnt/sdcard/tesseract/tessdata
  
-for f in tesseract-ocr/tessdata/eng.*; do
+for f in tessdata/eng.*; do
     TRAINEDDATA_FILENAME="/mnt/sdcard/tesseract/tessdata/`basename $f`"
 
     if [ `adb shell "if [ -f $TRAINEDDATA_FILENAME ]; then echo 1; fi"` ]; then
