688d487b11c2e8d17816fd91ba7de5a3c1cfd041
Robert Theis <robert.m.theis@gmail.com>
2017-08-12 19:17:04 -0700

    Don't build 64-bit ABIs by default

--- tess-two/jni/Application.mk
+++ tess-two/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := gnustl_static
-APP_ABI := armeabi armeabi-v7a x86 mips arm64-v8a x86_64 mips64
+APP_ABI := armeabi armeabi-v7a x86 mips # arm64-v8a x86_64 mips64
 APP_OPTIM := release
 APP_PLATFORM := android-9
 APP_CPPFLAGS += -fexceptions -frtti
