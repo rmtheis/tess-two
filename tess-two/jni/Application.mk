22adbe062a67b9273403c14f8fa8953f060d62c4
Robert Pösel <robyer@seznam.cz>
2019-01-08 11:07:04 +0100

    Increase minimum SDK to 16
    Currently that is the minimum supported version by build tools.

--- tess-two/jni/Application.mk
+++ tess-two/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_STL := c++_static
 APP_ABI := armeabi-v7a x86 arm64-v8a x86_64
 APP_OPTIM := release
-APP_PLATFORM := android-9
+APP_PLATFORM := android-16
 APP_CPPFLAGS += -fexceptions -frtti
 NDK_TOOLCHAIN_VERSION := clang
